fb22c40b5d2adb406f2b2d8f8469ab41076ca831
Shannnon <59317336+Shannnon@users.noreply.github.com>
2020-03-11 06:35:33 -0500

    dkfjggafg

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,112 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Playground exposing (..)
 
 
----- MODEL ----
+type State
+    = RunTheRace Number
+    | MoveToPodium ()
 
 
-type alias Model =
-    {}
+main =
+    game view update (RunTheRace -300)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+update : Computer -> State -> State
+update _ memory =
+    case memory of
+        RunTheRace x ->
+            if x > 290 then
+                MoveToPodium ()
+
+            else
+                RunTheRace (x + 3)
+
+        MoveToPodium () ->
+            memory
 
 
+view computer memory =
+    case memory of
+        RunTheRace x ->
+            let
+                time =
+                    computer.time
+            in
+            [ myRings time x
+                |> scale 0.2
+                |> moveRight 10
+                |> moveUp 100
+                |> moveX (wave 10 -10 10 time)
+            , medalStand |> scale 2 |> moveRight 150
+            , myTrack |> scale 2 |> moveRight 150
+            , myPeople time x |> scale 1 |> moveRight 2
+            ]
 
----- UPDATE ----
+        MoveToPodium () ->
+            [ rectangle red 20 20 ]
 
 
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+myPeople time state =
+    group
+        [ myPerson |> moveDown 150 |> moveX state
+        , myPerson |> moveDown 220 |> moveX state
+        , myPerson |> moveDown 300 |> moveX state
         ]
 
 
+myPerson =
+    group
+        [ rectangle black 10 45 |> moveUp 75 |> moveRight 8
+        , rectangle black 10 20 |> moveUp 50 |> moveRight 16 |> rotate 45
+        , rectangle black 10 20 |> moveUp 50 |> moveRight 0 |> rotate 140
+        , rectangle black 10 20 |> moveUp 70 |> moveRight 0 |> rotate 140
+        , rectangle black 10 20 |> moveUp 70 |> moveRight 16 |> rotate 45
+        , circle black 12 |> moveUp 95 |> moveRight 7.5
+        ]
 
----- PROGRAM ----
+
+medalStand =
+    group
+        [ rectangle orange 90 20
+        , rectangle orange 30 40 |> moveUp 25
+        , rectangle orange 30 30 |> moveUp 15 |> moveRight 30
+        , rectangle orange 30 20 |> moveUp 10 |> moveLeft 30
+        , words black "1"
+        , words black "2" |> moveRight 30
+        , words black "3" |> moveLeft 30
+        ]
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+myTrack =
+    group
+        [ rectangle red 1200 100 |> moveDown 90
+        , rectangle white 1000 5 |> moveDown 105
+        , rectangle white 1000 5 |> moveDown 70
+        , rectangle white 5 100 |> moveDown 90 |> moveRight 50
+        ]
+
+
+myRings time state =
+    group
+        [ myCircle blue (moveLeft 160) (moveUp 100)
+        , myCircle black (moveUp 100) (moveUp 0)
+        , myCircle red (moveUp 100) (moveRight 160)
+        , myCircle yellow (moveLeft 85) (moveUp 25)
+        , myCircle green (moveRight 85) (moveUp 25)
+        , rectangle black 600 25 |> moveDown 85
+        , rectangle black 600 25 |> moveUp 210
+        , rectangle black 25 300 |> moveLeft 288 |> moveUp 55
+        , rectangle black 25 300 |> moveRight 288 |> moveUp 55
+        ]
+
+
+myCircle myColor x y =
+    group
+        [ circle myColor 75
+            |> y
+            |> x
+        , circle white 55
+            |> y
+            |> x
+        ]
